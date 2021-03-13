44c1356e1a8d3131078f8179a25bc4b4125862ce
Guest <guest-a8u1wi@h29055.sgb.skir.kr>
2021-03-13 23:55:53 +0900

    210313

--- 210313_TESTFile.cpp
+++ 210313_TESTFile.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    cin.tie(0)->sync_with_stdio(0);
+	cout.tie(0);
+
+    int nTestCase = 0;
+    int CakeSize = 0;    
+
+    cin>>nTestCase;
     
+    while(nTestCase--)
+    {
+        cin>>CakeSize;
+        int nHowManyPour = 0;  
+        vector<bool> Cake(CakeSize,false);
+        
+        for(int i = 0; i<CakeSize; i++)
+        {                  
+            cin>>nHowManyPour;
+
+            for(int j = 0; j < nHowManyPour; j++)
+            {
+                Cake[i-j] = true;
+                if(i-j == 0)
+                    break;
+            }
+        }
+
+        for(int i=0; i<CakeSize; i++)
+        {
+            cout<<(Cake[i] ? "1" : "0")<<" ";
+        }
+        cout<<"\n";
+    }   
+
     return 0;
 }
